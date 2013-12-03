7224e776da3a7bb938ef00ca30da7ee06262e075
Satria akbar <akbarsatria36@yahoo.com>
2013-12-04 02:40:50 +0700

    uji coba

--- Pelita/src/com/example/pelita/arrayadapter/BlueAdapter.java
+++ Pelita/src/com/example/pelita/arrayadapter/BlueAdapter.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
+//asdasd
 public class BlueAdapter extends ArrayAdapter<String> {
         public BlueAdapter(Context context, int textViewResourceId,
                         ArrayList<String> objects) {
